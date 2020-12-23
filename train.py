8d4fdd7d78ef8406e2ae36d415f25252cf756bf7
joelma1 <64549018+joelma1@users.noreply.github.com>
2020-12-22 23:33:43 -0500

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 dataset = input('Enter dataset to be used (brain_mri, mnist, cifar, ddsm, lidc)\n')
 
 # load dataset
-x_train, y_train, x_test, y_test = load_dataset(dataset)
+x_train, y_train, x_test, y_test = load_dataset(dataset, path)
 
 # train model
 if dataset == 'mnist':
