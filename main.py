428b6feb3f3cd84e1fe7bc6bc14180954efc378b
joelma1 <64549018+joelma1@users.noreply.github.com>
2020-12-22 23:02:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 # Created (10/16/20)
 # Updated (12/22/20)
 
-
 # import dependencies
 import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
@@ -16,7 +15,6 @@ from data_loader import load_dataset
 from attacks import create_attack
 from configs import path_fig, path
 from utils import get_acc_preds, plot_attacks_acc
-
 
 # query user for dataset
 dataset = input('Enter dataset to be used (brain_mri, mnist, cifar, ddsm, lidc)\n')
@@ -36,10 +34,8 @@ print('y_train.shape', y_train.shape)
 print('x_test.shape', x_test.shape)
 print('y_test.shape', y_test.shape)
 
-
 # load model
 model = load_model(path + 'models/' + dataset + '/' + dataset + '_vgg16_model.h5')
-
 
 # load input shape
 if dataset == 'brain_mri':
@@ -53,29 +49,25 @@ elif dataset == 'cifar':
 elif dataset == 'lidc':
     input_shape = (224, 224, 3)
 
-
 # convert model to KerasClassifier
 classifier = KerasClassifier(clip_values=(0, 1), model=model, use_logits=False)
 
-# evaluate original classifier accuracy on training set
+# evaluate classifier accuracy on training set
 acc = get_acc_preds(classifier, x_train, y_train)
 print("\nAccuracy of original classifier on training set: %.2f%%" % (acc * 100))
 
-# Evaluate the classifier on the test set
+# Evaluate classifier accuracy on the test set
 acc = get_acc_preds(classifier, x_test, y_test)
 print("\nAccuracy of original classifier on test set: %.2f%%" % (acc * 100))
-
 
 # Craft adversarial samples
 attacker = create_attack(attack_type, classifier)
 x_test_adv = attacker.generate(x_test)
 x_train_adv = attacker.generate(x_train)
 
-
 # Evaluate the classifier on the adversarial test examples
 acc = get_acc_preds(classifier, x_test_adv, y_test)
 print("\nAccuracy of original classifier on adversarial test set: %.2f%%" % (acc * 100))
-
 
 # Plot classifier accuracy over attack strength for multiple specified attacks (in this case fgsm, bim, pgd)
 plot_attacks_acc(classifier, x_test, y_test, path_fig, dataset, 'vgg16_attacks_acc')
