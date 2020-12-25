180d658e5100e1db74ff7860658bda90c22f2d0f
joelma1 <64549018+joelma1@users.noreply.github.com>
2020-12-25 01:27:49 -0500

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@
 
 # import dependencies
 import tensorflow as tf
-from models.models import brain_vgg_model, mnist_vgg_model, cifar_vgg_model, lidc_vgg_model, ddsm_vgg_model
+from models.models import medical_vgg_model, mnist_vgg_model, cifar_vgg_model
 from data_loader import load_dataset
 from utils import get_acc_preds, plot_tv
 from configs import path
@@ -38,21 +38,21 @@ elif dataset == 'cifar':
 elif dataset == 'brain_mri':
     es = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
     input_shape = [224, 224, 3]
-    model = brain_vgg_model(input_shape)
+    model = medical_vgg_model(input_shape)
     history = model.fit(x_train, y_train, batch_size=50, epochs=100, callbacks=[es], validation_data=(x_test, y_test), verbose=True)
 
 
 elif dataset == 'ddsm':
     es = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, mode='auto')
     input_shape = [116, 116, 3]
-    model = ddsm_vgg_model(input_shape)
+    model = medical_vgg_model(input_shape)
     history = model.fit(x_train, y_train, batch_size=50, epochs=100, callbacks=[es], validation_data=(x_test, y_test), verbose=True)
 
 
 elif dataset == 'lidc':
     es = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=1, mode='auto')
     input_shape = [224, 224, 3]
-    model = lidc_vgg_model(input_shape)
+    model = medical_vgg_model(input_shape)
     history = model.fit(x_train, y_train, epochs=200, callbacks=[es], validation_data=(x_test, y_test), verbose=True)
 
 # save model
